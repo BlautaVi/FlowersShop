6e49d3bd923c7a798794e1b72a19626503b386c1
Vi <blautavika@gmail.com>
2025-03-29 14:37:55 +0200

    з'єднала кілька сторінок через кнопки

--- app/src/main/java/com/example/flowersshop/Registration.kt
+++ app/src/main/java/com/example/flowersshop/Registration.kt
@@ -1,6 +1,8 @@
 package com.example.flowersshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class Registration : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val mainPage_b = findViewById<Button>(R.id.reg_b)
+        mainPage_b.setOnClickListener(){
+            val intent = Intent(this, main_page::class.java)
+            startActivity(intent)
         }
     }
 }
