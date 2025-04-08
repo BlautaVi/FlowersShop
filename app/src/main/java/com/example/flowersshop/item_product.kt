e06fc2d4a62b2345824ef28395ce43b19389a9a1
Vi <blautavika@gmail.com>
2025-04-08 20:39:05 +0300

    спробувала додати аунтефікацію на початку

--- app/src/main/java/com/example/flowersshop/item_product.kt
+++ app/src/main/java/com/example/flowersshop/item_product.kt
@@ -1,28 +1,20 @@
 package com.example.flowersshop
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Registration : AppCompatActivity() {
+class item_product : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registration)
+        setContentView(R.layout.activity_item_product)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val mainPage_b = findViewById<Button>(R.id.reg_b)
-
-        mainPage_b.setOnClickListener(){
-            val intent = Intent(this, main_page::class.java)
-            startActivity(intent)
         }
     }
 }
