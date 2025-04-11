58cd064968a2792bd31fec5fb057958354324554
BlautaVi <blautavika@gmail.com>
2025-04-11 23:21:51 +0300

    реєстрація + апгрейд сторінки для гостя

--- app/src/main/java/com/example/flowersshop/Registration.kt
+++ app/src/main/java/com/example/flowersshop/Registration.kt
@@ -3,26 +3,53 @@ package com.example.flowersshop
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Registration : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var registerBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registration)
+        auth = FirebaseAuth.getInstance()
+        email = findViewById(R.id.enter_login)
+        password = findViewById(R.id.enter_passwd)
+        registerBtn = findViewById(R.id.reg_b)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val mainPage_b = findViewById<Button>(R.id.reg_b)
+        registerBtn.setOnClickListener {
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
 
-        mainPage_b.setOnClickListener(){
-            val intent = Intent(this, main_page::class.java)
-            startActivity(intent)
+            if (emailText.isNotEmpty() && passwordText.isNotEmpty()) {
+                registerUser(emailText, passwordText)
+            } else {
+                Toast.makeText(this, "Заповніть всі поля!", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Успішно зареєстровано!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, main_page::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "Помилка...", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
