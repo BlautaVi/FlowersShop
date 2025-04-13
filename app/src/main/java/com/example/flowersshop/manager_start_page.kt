ad0183108dfcc12a29078220d7801a76f30cf41d
BlautaVi <blautavika@gmail.com>
2025-04-13 23:10:12 +0300

    пару моментів

--- app/src/main/java/com/example/flowersshop/manager_start_page.kt
+++ app/src/main/java/com/example/flowersshop/manager_start_page.kt
@@ -1,12 +1,15 @@
 package com.example.flowersshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class manager_start_page : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,18 @@ class manager_start_page : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val add_item = findViewById<Button>(R.id.man_add_items_b)
+        val see_items = findViewById<Button>(R.id.man_see_all_b)
+        val see_orders = findViewById<Button>(R.id. man_all_orders_b)
+        add_item.setOnClickListener {
+            startActivity(Intent(this, manager_add_item::class.java))
+        }
+        see_items.setOnClickListener {
+            startActivity(Intent(this, main_page::class.java))
+        }
+       // see_orders.setOnClickListener {
+         //   startActivity(Intent(this, main_page::class.java))
+           // finish()
+        //}
     }
 }
